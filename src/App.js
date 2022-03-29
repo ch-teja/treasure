b2af23af645d0876b941cd23dbe654c077f066a9
teja538 <saisree538@gmail.com>
2022-03-29 23:26:31 +0530

    home basic

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>hii</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+const Home = () =>
+{
+    return(
+        <div>
+            <h2>I am home comp</h2>
+        </div>
+    )
+}
+export {Home}
